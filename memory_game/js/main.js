346dadc300f2a766965bd850884cfa36f361c0fe
Sam Bernal <sambernal@konnichiwa-sams-computer-desu.local>
2019-08-31 15:54:01 -0400

    created more game logic from unit 8 of fundamentals course

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,22 @@
 // console.log("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-console.log("User Flipped " + cardOne);
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("You found a match!");
+	} 
+	else {
+		alert("Sorry, try again!");
+	}	
+}
